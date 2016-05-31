f7498d9788974b8f7d4eb638c7d633f36654fdd1
John Crooks <johnmcrooks@gmail.com>
2016-05-31 12:00:08 -0400

    Week 3 Day 2: Adress Bar updates as the webpage changes.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,19 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable, ChangeListener {
     @FXML
     TextField addresBar;
 
@@ -30,8 +36,7 @@ public class Controller {
     }
 
     public void onBack(){
-        try{
-            view.getEngine().getHistory().go(-1); // method chaining... Get the web engine, go to it's history and go back by setting -1
+        try{view.getEngine().getHistory().go(-1); // method chaining... Get the web engine, go to it's history and go back by setting -1
         }catch (Exception e){}
 
     }
@@ -39,5 +44,17 @@ public class Controller {
     public void onForward(){
        try{ view.getEngine().getHistory().go(1);
     }catch (Exception e) {}
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        view.getEngine().getLoadWorker().stateProperty().addListener(this);
+    }
+
+    @Override
+    public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+        String adress = view.getEngine().getLocation();
+        addresBar.setText(adress);
     }
 }
